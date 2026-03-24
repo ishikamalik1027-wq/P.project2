fa9da936a8cf7b209b2f5918f07a115d55e418be
Ishika Malik <ishikamalik1027@gmail.com>
2026-03-24 19:55:07 +0530

    fixed auth header

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
 
-  const token = req.header("x-auth-token");
+  const token = req.header("Authorization");
 
   if (!token) {
     return res.status(401).json({ msg: "No token" });
